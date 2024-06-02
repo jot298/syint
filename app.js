86db5cc00c1f04dba17ea3f937ca2dccd4ca6e24
Johannes Strobl <johannesstrobl18@gmail.com>
2024-06-02 15:12:49 +0200

    Added first saves to DB, changed API, changed DB Format

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const timers = require("timers-promises");
-const { Client } = require('pg');
+const { Client } = require("pg");
 
 const options = { method: "GET", headers: { accept: "application/json" } };
 
@@ -8,7 +8,8 @@ const loadData = async () => {
   console.log("Starting Loading Loop: ");
   while (true) {
     fetch(
-      "https://api.tomorrow.io/v4/weather/realtime?location=toronto&apikey=CF8QTRhpprm11YkrniU6d1SeqhaQcpkP",
+      //"https://api.tomorrow.io/v4/weather/realtime?location=Vienna&apikey=CF8QTRhpprm11YkrniU6d1SeqhaQcpkP",
+      "http://api.weatherapi.com/v1/current.json?key=6cec91a8d57c4d72a11125538240206&q=Vienna&aqi=no",
       options
     )
       .then((response) => response.json())
@@ -28,15 +29,32 @@ const loadData = async () => {
 const save = async (response) => {
   // docker run -e POSTGRES_PASSWORD=secret -d -p 5432:5432 postgres
   const client = new Client({
-    user: 'postgres',
-    password: 'secret',
+    user: "postgres",
+    password: "secret",
     port: 5432,
   });
   await client.connect();
-  const res = await client.query('SELECT $1::text as connected', ['Connection to postgres successful!']);
-  console.log(res.rows[0].connected);
+  /*const res = await client.query("SELECT $1::text as connected", [
+    "Connection to postgres successful!",
+  ]);
+  console.log(res.rows[0].connected);*/
+  const query = {
+    text: "INSERT INTO weather_data (place, created_at, temperature, uv_index, wind_direction, wind_speed) VALUES($1, $2, $3, $4, $5, $6)",
+    values: [
+      response.location.name,
+      response.location.localtime,
+      response.current.temp_c,
+      response.current.uv,
+      response.current.wind_dir,
+      response.current.wind_kph,
+    ],
+  };
+  const res = await client.query(query);
+  console.log(res.rows[0]);
+  console.log("Successfully Saved dataset");
+
   await client.end();
-}
+};
 
 const sendData = async () => {
   console.log("Starting endless loop: ");
--- database_files/weather_data.sql
+++ database_files/weather_data.sql
@@ -1,9 +1,9 @@
 create table if not exists weather_data (
-    id uuid primary key,
+    id SERIAL primary key,
     place text not null,
     created_at timestamp not null,
     temperature numeric(5, 2) not null,
     uv_index integer,
-    wind_direction integer,
+    wind_direction text not null,
     wind_speed numeric(5, 2)
 );
