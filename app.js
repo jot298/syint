5e2315d6e67c185e9e6ca013cfdbe05b7fe904ec
Johannes Strobl <johannesstrobl18@gmail.com>
2024-06-02 15:45:50 +0200

    Added config file for urls and tokens. Finished implementation of sending to webhook and providing last item via url /weather_vienna

--- app.js
+++ app.js
@@ -1,28 +1,50 @@
 const express = require("express");
 const timers = require("timers-promises");
 const { Client } = require("pg");
+const config = require("./config.json");
 
 const options = { method: "GET", headers: { accept: "application/json" } };
 
 const loadData = async () => {
-  console.log("Starting Loading Loop: ");
   while (true) {
+    const fetch_api =
+      "http://api.weatherapi.com/v1/current.json?key=" +
+      config.API_KEY +
+      "&q=Graz&aqi=no";
     fetch(
       //"https://api.tomorrow.io/v4/weather/realtime?location=Vienna&apikey=CF8QTRhpprm11YkrniU6d1SeqhaQcpkP",
-      "http://api.weatherapi.com/v1/current.json?key=6cec91a8d57c4d72a11125538240206&q=Vienna&aqi=no",
+      fetch_api,
       options
     )
       .then((response) => response.json())
       .then((response) => {
-        console.log("Current Weatherdata:");
+        console.log("Current Weatherdata Graz:");
         console.log(response);
-
         save(response);
       })
       .catch((err) => console.error(err));
 
     // Wait 20 sec before next call
-    await timers.setTimeout(20000);
+    await timers.setTimeout(10000);
+    const fetch2_api =
+      "http://api.weatherapi.com/v1/current.json?key=" +
+      config.API_KEY +
+      "&q=Vienna&aqi=no";
+    fetch(
+      //"https://api.tomorrow.io/v4/weather/realtime?location=Vienna&apikey=CF8QTRhpprm11YkrniU6d1SeqhaQcpkP",
+      fetch2_api,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        console.log("Current Weatherdata Vienna:");
+        console.log(response);
+        save(response);
+      })
+      .catch((err) => console.error(err));
+
+    // Wait 20 sec before next call
+    await timers.setTimeout(10000);
   }
 };
 
@@ -60,7 +82,31 @@ const sendData = async () => {
   console.log("Starting endless loop: ");
   while (true) {
     await timers.setTimeout(18000);
-    console.log("sending data");
+    // docker run -e POSTGRES_PASSWORD=secret -d -p 5432:5432 postgres
+    const client = new Client({
+      user: "postgres",
+      password: "secret",
+      port: 5432,
+    });
+    await client.connect();
+    const query = {
+      text: "SELECT * FROM weather_data WHERE place = 'Graz' ORDER BY created_at DESC LIMIT 1;",
+      values: [],
+    };
+    const res = await client.query(query);
+    console.log(res.rows[0]);
+    console.log("Successfully Saved dataset");
+
+    await client.end();
+    const url = config.WEBHOOK_URL;
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(res.rows[0]),
+    });
   }
 };
 
@@ -69,10 +115,25 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-var current_data = [];
-
 app.listen(PORT, () => {
   console.log("Server Listening on PORT:", PORT);
+});
+
+app.get("/weather_vienna", async (req, res) => {
+  const client = new Client({
+    user: "postgres",
+    password: "secret",
+    port: 5432,
+  });
+  await client.connect();
+  const query = {
+    text: "SELECT * FROM weather_data WHERE place = 'Vienna' ORDER BY created_at DESC LIMIT 1;",
+    values: [],
+  };
+  const resp = await client.query(query);
+  await client.end();
+
+  res.json(resp.rows[0]);
 });
 
 loadData();
