cd2907e9620c9e7f8d495024e5dfe769336806a0
Dotraxx <83698638+Dotraxx@users.noreply.github.com>
2024-06-04 18:59:44 +0200

    Fixed Frontend

--- frontend/location/location.js
+++ frontend/location/location.js
@@ -45,12 +45,12 @@ function loadTemperatureData() {
       new Chart(document.getElementById("temperatureCanvas"), {
         type: "line",
         options: {
-          /* scales: {
+          scales: {
             y: {
-              min: Math.floor(formattedTemperatureData["min"]),
-              max: Math.ceil(parseFloat(formattedTemperatureData["max"])),
+              min: Math.floor(formattedTemperatureData["min"])-1,
+              max: Math.ceil(parseFloat(formattedTemperatureData["max"]))+1,
             },
-          },*/
+          },
         },
         data: {
           labels: formattedTemperatureData["graphLabels"],
--- frontend/main-page.js
+++ frontend/main-page.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 function loadLocations() {
   $.ajax({
     type: "GET",
-    url: "http://localhost:3000/locations",
+    url: "http://127.0.0.1:3000/locations",
     cache: false,
     dataType: "json",
     success: function (response) {
@@ -13,63 +13,70 @@ function loadLocations() {
       for (let i = 0; i < response["data"].length; i++) {
         let location = response["data"][i];
 
-        // translates degrees into specific wind direction
-        let windDirection;
-        if (
-          (location["wind-direction"] >= 0 &&
-            location["wind-direction"] < 22.5) ||
-          (location["wind-direction"] >= 337.5 &&
-            location["wind-direction"] <= 360)
-        ) {
-          windDirection = "Nord";
-        } else if (
-          location["wind-direction"] >= 22.5 &&
-          location["wind-direction"] < 67.5
-        ) {
-          windDirection = "Nord-West";
-        } else if (
-          location["wind-direction"] >= 67.5 &&
-          location["wind-direction"] < 112.5
-        ) {
-          windDirection = "West";
-        } else if (
-          location["wind-direction"] >= 112.5 &&
-          location["wind-direction"] < 157.5
-        ) {
-          windDirection = "Süd-West";
-        } else if (
-          location["wind-direction"] >= 157.5 &&
-          location["wind-direction"] < 202.5
-        ) {
-          windDirection = "Süd";
-        } else if (
-          location["wind-direction"] >= 202.5 &&
-          location["wind-direction"] < 247.5
-        ) {
-          windDirection = "Süd-Ost";
-        } else if (
-          location["wind-direction"] >= 247.5 &&
-          location["wind-direction"] < 292.5
-        ) {
-          windDirection = "Ost";
-        } else if (
-          location["wind-direction"] >= 292.5 &&
-          location["wind-direction"] < 337.5
-        ) {
-          windDirection = "Nord-Ost";
+        // translates abbreviation into full name and corresponding degrees
+        let windDirection = "";
+        switch (location["wind_direction"]) {
+          case "N":
+            windDirection = "Norden (0 °C)";
+            break;
+            case "NNE":
+            windDirection = "Nord-Nord-Osten (22,5 °C)";
+            break;
+            case "NE":
+            windDirection = "Nord-Osten (45 °C)";
+            break;
+            case "ENE":
+            windDirection = "Ost-Nord-Osten (67,5 °C)";
+            break;
+            case "E":
+            windDirection = "Osten (90 °C)";
+            break;
+            case "ESE":
+            windDirection = "Ost-Süd-Osten (112,5 °C)";
+            break;
+            case "SE":
+            windDirection = "Süd-Osten (135 °C)";
+            break;
+            case "SSE":
+            windDirection = "Süd-Süd-Osten (157,5 °C)";
+            break;
+            case "S":
+            windDirection = "Süden (180 °C)";
+            break;
+            case "SSW":
+            windDirection = "Süd-Süd-Westen (202,5 °C)";
+            break;
+            case "SW":
+            windDirection = "Süd-Westen (225 °C)";
+            break;
+            case "WSW":
+            windDirection = "West-Süd-Westen (247,5 °C)";
+            break;
+            case "W":
+            windDirection = "Westen (270 °C)";
+            break;
+            case "WNW":
+            windDirection = "West-Nord-Westen (292,5 °C)";
+            break;
+            case "NW":
+            windDirection = "Nord-Westen (315 °C)";
+            break;
+            case "NNW":
+            windDirection = "Nord-Nord-Westen (337,5 °C)";
+            break;
         }
 
         // translates uv index into words
         let uvIndexDesc = "";
-        if (location["uv-index"] <= 2) {
+        if (location["uv_index"] <= 2) {
           uvIndexDesc = "niedrig";
-        } else if (location["uv-index"] <= 5) {
+        } else if (location["uv_index"] <= 5) {
           uvIndexDesc = "mäßig";
-        } else if (location["uv-index"] <= 7) {
+        } else if (location["uv_index"] <= 7) {
           uvIndexDesc = "hoch";
-        } else if (location["uv-index"] <= 10) {
+        } else if (location["uv_index"] <= 10) {
           uvIndexDesc = "sehr hoch";
-        } else if (location["uv-index"] >= 11) {
+        } else if (location["uv_index"] >= 11) {
           uvIndexDesc = "extrem";
         }
 
@@ -82,13 +89,11 @@ function loadLocations() {
             ' °C</p><p class="card-text">UV-Index: ' +
             uvIndexDesc +
             " (" +
-            location["uv-index"] +
+            location["uv_index"] +
             ')</p><p class="card-text">Windrichtung: ' +
             windDirection +
-            " (" +
-            location["wind-direction"] +
-            '°)</p><p class="card-text">Windstärke: ' +
-            location["wind-speed"] +
+            '</p><p class="card-text">Windstärke: ' +
+            location["wind_speed"] +
             ' m/s</p><a href="/location/location.html?id=' +
             location["id"] +
             '" class="btn btn-primary">Historische Daten</a></div></div></div>'
